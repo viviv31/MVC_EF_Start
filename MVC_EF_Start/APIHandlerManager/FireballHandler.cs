18721d090b826b6cf6256f5985e4d95f0d0b9b9c
Viviana Milla Angeles <vivianamilla@usf.edu>
2020-04-24 10:24:31 -0400

    New Branch to check

---
+++ MVC_EF_Start/APIHandlerManager/FireballHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using static MVC_EF_Start.Models.Fireball;
+using MVC_EF_Start.Models;
+using MVC_EF_Start.DataAccess;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+
+namespace MVC_EF_Start.APIHandlerManager
+{
+    public class FireballHandler
+    { 
+        static string BASE_URL = "https://ssd-api.jpl.nasa.gov/fireball.api";
+        static string API_KEY = "2DbgzjUw3YVFzbsMRW4FYXpknpTYPrSzMH1Rf8Hz"; //Add your API key here inside ""
+        HttpClient httpClient;
+
+    public FireballHandler()
+    {
+        httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Accept.Clear();
+        httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+        httpClient.DefaultRequestHeaders.Accept.Add(
+        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+    }
+
+    public FireballObject GetFireballObjects()
+    {
+        string FIREBALL_API_PATH = BASE_URL;
+        string fireballRaw = "";
+        FireballObject fireballData = null;
+        httpClient.BaseAddress = new Uri(FIREBALL_API_PATH);
+
+        try
+        {
+            HttpResponseMessage response = httpClient.GetAsync(FIREBALL_API_PATH).GetAwaiter().GetResult();
+
+            if (response.IsSuccessStatusCode)
+            {
+                fireballRaw = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            }
+
+            if (!fireballRaw.Equals(""))
+            {
+                fireballData = JsonConvert.DeserializeObject<FireballObject>(fireballRaw);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        return fireballData;
+    }
+}
+}
+
---
+++ MVC_EF_Start/Models/Fireball.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_EF_Start.Models
+{
+
+    public class FireballObject
+    {
+        public string count { get; set; }
+        public Signaturef signature { get; set; }
+        public Fireball[] data { get; set; }
+    }
+
+    public class Signaturef
+    {
+        public string source { get; set; }
+        public string version { get; set; }
+    }
+
+    public class Fireball
+    {
+        public string neo1kmScore { get; set; }
+        public string lastRun { get; set; }
+        public string uncP1 { get; set; }
+        public string dec { get; set; }
+        public string neoScore { get; set; }
+        public string rating { get; set; }
+        public string rate { get; set; }
+        public string unc { get; set; }
+        public string phaScore { get; set; }
+        public string ra { get; set; }
+        public string elong { get; set; }
+        public string nObs { get; set; }
+        public string arc { get; set; }
+        public string tEphem { get; set; }
+        public string objectName { get; set; }
+        public string tisserandScore { get; set; }
+        public string caDist { get; set; }
+        public string vInf { get; set; }
+        public string H { get; set; }
+        public string rmsN { get; set; }
+        public string ieoScore { get; set; }
+        public string geocentricScore { get; set; }
+        [Key]
+        public string moid { get; set; }
+        public string Vmag { get; set; }
+    }
+
+}
